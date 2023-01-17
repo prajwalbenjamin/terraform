058923ca82d644196be16fd914a54d60f175e009
prajwalbenjamin <64261163+prajwalbenjamin@users.noreply.github.com>
2023-01-17 15:33:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      version = "3.37.0"
       source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-
- }
+  features {}
   subscription_id   = "e61c9c1e-1f3b-4a27-bd84-1631b0c60c12"
   tenant_id         = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4"
 }
