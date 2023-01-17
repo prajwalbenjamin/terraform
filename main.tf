4c1995e60285c3f6c6705e46421e630f4a848ecc
prajwalbenjamin <64261163+prajwalbenjamin@users.noreply.github.com>
2023-01-17 17:11:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   subscription_id   = "e61c9c1e-1f3b-4a27-bd84-1631b0c60c12"
   tenant_id         = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4"
   client_id         = "6d889d1a-45f0-479c-9a8b-b3c717602317"
-  client_secret     = "Y408Q~FxXHg7NW7XohAFN-j3XAvBZCpeSbT.Uc-w"
+  client_secret     = "8d0e5953-e9eb-4343-9a61-d1b07afd9a83"
 }
 
 resource "azurerm_virtual_network" "vnet" {
